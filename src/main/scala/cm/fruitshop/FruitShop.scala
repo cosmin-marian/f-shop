de6a98f46e50769839e2cf4491fd2e4d88d7dd8a
Cosmin Marian <cosmin>
2017-05-09 17:05:17 +0300

    Example app moved to its own file
    Change-Id: Ic97f0680d877cf94fd8a445a0286fa8dcf4f548c

--- src/main/scala/cm/fruitshop/FruitShop.scala
+++ src/main/scala/cm/fruitshop/FruitShop.scala
@@ -7,19 +7,3 @@ object FruitShop {
   }
 }
 
-object FruitShopExample {
-
-  def main(args: Array[String]): Unit = {
-    val prices: Map[FruitShop.Item, Double] = Map(
-      "orange" → 0.25,
-      "apple" → 0.6
-    )
-
-    val items = List("apple", "apple", "orange", "apple")
-
-    println(s"Total price for $items is ${FruitShop.computeListPrice(prices, items)}")
-
-  }
-}
-
-
